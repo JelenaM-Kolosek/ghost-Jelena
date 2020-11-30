0e3f1cd7d5cc8e2a31c8875249be0d8c7e23a325
JelenaM-Kolosek <jelenam@kolosek.io>
2020-11-30 15:21:51 +0100

    [175846490] default scope added, author scope changed

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,6 +1,6 @@
 class Story < ApplicationRecord
+  default_scope { order(created_at: :desc) }
   belongs_to :user
 
-  scope :stories_desc, -> { order(created_at: :desc) }
-  scope :stories_desc_author, ->(user) { where(user_id: user.id).order(created_at: :desc) }
+  scope :stories_author, ->(user) { where(user_id: user.id) }
 end
